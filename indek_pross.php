fe6f92ad1d3052342d0ad5aa61173d38596b9985
KevinNuriawan <120350473+KevinNuriawan@users.noreply.github.com>
2024-11-09 10:57:51 +0700

    ubah nama

---
+++ indek_pross.php
@@ -0,0 +1,67 @@
+<?php
+include("koneksi.php");
+
+$nidn = $_POST['nidn'];
+$name = $_POST['nama'];
+$rumpun = $_POST['rumpun'];
+$tempat = $_POST['tempat'];
+$tanggal = $_POST['tanggal'];
+$email = $_POST['email'];
+
+$simpan = "INSERT INTO tb_dosen (nidn,nama,rumpun,tempat,tanggal,email) VALUES ('$nidn','$name','$rumpun','$tempat','$tanggal','$email')";
+mysqli_query($koneksi,$simpan);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <div clas="row mt-">
+        <div class="col-6 m-auto">
+            <div class="card">
+            <div class="card-header text-center">
+                <h3>Form Dosen</h3>
+            </div>
+            <div class="card-body">
+            <table class="table">
+            <tbody>
+                <tr>
+                    <td scope="row">Nidn</td>
+                    <th>: <?=$nidn?></th>
+                </tr>
+                <tr>
+                    <td scope="row">Nama Lengkap</td>
+                    <th>: <?=$name?></th>
+                </tr>
+                <tr>
+                    <td scope="row">Rumpun</td>
+                    <th>: <?=$rumpun?></th>
+                </tr>
+                <tr>
+                    <td scope="row">Tempat Lahir</td>
+                    <th>: <?=$tempat?></th>
+                </tr>
+                <tr>
+                    <td scope="row">Tanggal Lahir</td>
+                    <th>: <?=$tanggal?></th>
+                </tr>
+                <tr>
+                    <td scope="row">Email</td>
+                    <th>: <?=$email?></th>
+                </tr>
+            </tbody>
+            </table>
+            <a href="indek.php" class="btn btn-warning btn-sm">Kembali</a>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
