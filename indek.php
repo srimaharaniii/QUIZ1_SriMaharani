1193d7fb8047e90906dbfb0063082f7acefc1593
KevinNuriawan <120350473+KevinNuriawan@users.noreply.github.com>
2024-11-09 10:31:45 +0700

    ubah nama

--- indek.php
+++ indek.php
@@ -4,8 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
+<div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Nidn</label>
+                        <input type="text" name="nidn" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text"></div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Nama Lengkap</label>
+                        <input type="text" name="name" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Rumpun</label>
+                        <select name="rumpun" id="" class="from-control">
+                            <option value="">-Pilih Rumpun</option>
+                            <option value="Islam">Komputer</option>
+                            <option value="Kristen">Akutansi</option>
+                            <option value="Katolik">Manajemen</option>
+                            <option value="Budha">Perpajakan</option>
+                            <option value="Hindu">Bisnis</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Tempat Lahir</label>
+                        <input type="text" name="tempat" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Tanggal Lahir</label>
+                        <input type="date" name="tanggal"class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputPassword1" class="form-label">Email</label>
+                        <input type="email" name="email"class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="reset" class="btn btn-info">reset</button>
+                </form>
+            </div>
+            </div>
+        </div>
+    </div>
+</div>
     
 </body>
 </html>
